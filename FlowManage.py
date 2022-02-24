275a31e5d76da63a0ed9a7f3032191abe523439c
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2022-02-24 14:21:35 +0100

    update flowmanage

--- FlowManage.py
+++ FlowManage.py
@@ -7,7 +7,7 @@ def main():
     """
     # Parse command-line arguments
     if '--help' in sys.argv:
-        fm.con.print("[blue underline]Usage:[/] [green]python FlowManage.py --options")
+        fm.con.print("[blue underline]Usage[/][blue]: [green]python FlowManage.py --options")
         fm.con.print("\n[magenta]FlowManage will run all modules in order if no options are specified.")
         fm.con.print("[magenta]The order of modules is: (1)intention, (2)airspace, (3)scenario.")
         fm.con.print("\n[red underline]Options:")
