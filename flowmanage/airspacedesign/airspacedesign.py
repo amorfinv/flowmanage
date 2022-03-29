fe26c8c65285dafd3c22f99bef04f44d77577eea
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2022-03-29 21:31:32 +0200

    add heading constrained airspace

--- flowmanage/airspacedesign/airspacedesign.py
+++ flowmanage/airspacedesign/airspacedesign.py
@@ -24,13 +24,15 @@ class AirspaceDesign:
         self.heading_airspace = fm.settings.heading_airspace
         
         self.airspace_filepath = fm.settings.airspace_filepath
+
+        self.heading_constrained = fm.settings.heading_constrained
         
         # Initialize the airspace config dictionary
         self.airspace_config = {}
         self.airspace_info = {}
         
     def process(self) -> None:
-        
+
         # step 1.a: initialize the airspace info
         layer_heights = list(range(self.min_height, self.max_height + self.layer_spacing, self.layer_spacing))
         self.airspace_info = {'levels': layer_heights, 'spacing': self.layer_spacing}
@@ -54,6 +56,15 @@ class AirspaceDesign:
                 # not open airspace
                 layers_levels, pattern = self.build_layer_airspace_dict(layer_heights, stack_list, 'levels')
                 layers_range, pattern = self.build_layer_airspace_dict(layer_heights, stack_list, 'range')
+
+                # here check if there is a constrained heading airspace
+                if self.heading_constrained:
+                    # modify the layers_levels and layers_range dictionaries
+                    layers_levels = self.constrained_heading(layer_heights, layers_levels, pattern, stack_list)
+                    layer_heights_ranges = [f'{x-self.layer_spacing/2}-{x+self.layer_spacing/2}' for x in layer_heights]
+                    layers_range = self.constrained_heading(layer_heights_ranges, layers_range, pattern, stack_list)
+
+
                 self.airspace_config[stack_name] = {'levels': layers_levels, 'range': layers_range, 'pattern': pattern}
             else:
                 # open airspace (headings)
@@ -377,3 +388,135 @@ class AirspaceDesign:
         angle_dict = {'ranges': angle_dict_ranges, 'center': angle_dict_center}
 
         return height_dict, angle_dict, angle_ranges
+
+    def constrained_heading(self, layer_heights, layers_levels, pattern, stack_list):
+        """ This function creates a constrained heading pattern. That extends the dictionary
+        created by build_layer_airspace_dict. It adds another dimmension to this dictionary.
+        It looks at min_angle_constrained, max_angle_constrained, and angle_spacing_constrained.
+        to subdivide the stack of layers into specific heading ranges. If it can't fit the
+        heading ranges equally into the stack the layers that don't fit will be made free layers.
+        For example. 
+        -assume 5 heading ranges (spacing 72 degrees). 
+        -asssume each heading range will get a C, T and F layer.
+        -This gives a total of 15 layers.
+        -However if we actually have 16 layers 30,60...480, then the last layer will be a free layer.
+        Args:
+            layer_heights (list): list of layer heights
+            layers_levels (dictionary): layer levels dictionary from build_layer_airspace_dict
+            pattern (list): layer list showing the pattern
+            stack_list (list): layer stack from settings.
+        Returns:
+            [dictionary]: constrained heading dictionary that is similar to output
+            of build_layer_airspace_dict. However, there is a new dimension for the
+            heading range.
+
+        """
+        # get the angle pattern
+        start_ind = fm.settings.min_angle_constrained
+        end_ind = fm.settings.max_angle_constrained + fm.settings.angle_spacing_constrained
+
+        # get list of angles
+        angles = [idx for idx in range(start_ind, end_ind, fm.settings.angle_spacing_constrained)]
+        
+        angle_ranges = []
+        
+        for idx in range(len(angles) - 1):
+            angle_ranges.append(f'{angles[idx]}-{angles[idx + 1]}')
+        # if the number of layers is greater than angle_ranges*stack_list, then label the
+        # end layers as Free layers ('F')
+        if len(pattern) > len(angle_ranges) * len(stack_list):
+            free_layers = len(pattern) - len(angle_ranges) * len(stack_list)
+
+            # label the end layers as free layers
+            for i in range(free_layers):
+                pattern[~i] = 'F'
+
+    
+        # make a layer dict with the angle_range as key and an empty list as value
+        # then fill list with the heights for each angle range
+        layer_dict = {x:[] for x in angle_ranges}
+        for idx, angle_range in enumerate(angle_ranges):
+            # get the layer heights as multiples of stack
+            selected_height_idx = list(range(idx * len(stack_list), (idx + 1) * len(stack_list)))
+            
+            layer_dict[angle_range] = [layer_heights[i] for i in selected_height_idx]
+
+        # make a new dict that has the layer_levels dict for each angle range
+        new_dict = {}
+        for angle_range in angle_ranges:
+            new_dict[angle_range] = deepcopy(layers_levels)
+        # new_dict = {x:layers_levels for x in angle_ranges}
+
+        for angle_range, levels in new_dict.items():
+            # relevant layer heights
+            rel_heights = layer_dict[angle_range]
+
+            # other heights
+            other_heights = [i for i in layer_heights if i not in rel_heights]
+            
+            min_height = min(layer_dict[angle_range])
+            max_height = max(layer_dict[angle_range])
+
+            # loop through the relevant heights
+            for rel_height in rel_heights:
+
+                # loop through the first 7 entries of levels[rel_height]
+                for idx, level in enumerate(levels[rel_height][:7]):
+                    if isinstance(level, str) or min_height <= level <= max_height:
+                        continue
+                    else:
+                        # get the closest layer height
+                        levels[rel_height][idx] = ""
+
+            
+            # loop through the other heights
+            for other_height in other_heights:
+                # first see if it is below or above the relevant heights
+                if other_height < min_height:
+                    # loop through the first 7 entries of levels[rel_height]
+                    for idx, level in enumerate(levels[other_height][:7]):
+                        
+                        # make all layers free
+                        if idx == 0:
+                            levels[other_height][idx] = "F"
+                        # even values are top
+                        elif not idx%2:
+                            levels[other_height][idx] = rel_heights[int(idx/2 -1)]
+                        else:
+                            # get the closest layer height
+                            levels[other_height][idx] = ""
+                
+                else:
+                    # values above the relevant heights
+                    # loop through the first 7 entries of levels[rel_height]
+                    for idx, level in enumerate(levels[other_height][:7]):
+                        # make all layers free
+                        if idx == 0:
+                            levels[other_height][idx] = "F"
+                        # odd values are bottom
+                        elif idx%2:
+                            levels[other_height][idx] = rel_heights[int((idx-1)/2)]
+                        else:
+                            # get the closest layer height
+                            levels[other_height][idx] = ""
+
+
+            # check the ground level
+            if self.ground_level_layer:
+                layer_key = list(set(layers_levels.keys()) - set(layer_heights))[0]
+                # loop through the first 7 entries of levels[rel_height]
+                for idx, level in enumerate(levels[layer_key][:7]):
+                    # make all layers free
+                    if idx == 0:
+                        levels[layer_key][idx] = ""
+                    # even values are top
+                    elif not idx%2:
+                        levels[layer_key][idx] = rel_heights[int((idx-1)/2)]
+                    else:
+                        # get the closest layer height
+                        levels[layer_key][idx] = ""
+
+            # create a new dict that has the layer_levels dict for each angle range
+            new_dict[angle_range] = levels
+        
+        return new_dict
